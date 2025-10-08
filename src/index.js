75c6ab09b82e216a9a5952167dac86db49dcac05
brobro332 <332bro@naver.com>
2025-10-09 00:24:23 +0900

    feat: test rerendering with repeated root.render call

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+/*
+ * [앱 리렌더링]
+ * 1. root.render() 메서드를 반복 호출함으로써 렌더링을 반복한다.
+ * 2. 이때 업데이트가 필요한 부분 HTML 요소만 교체되는 것을 확인할 수 있다.
+ *    이를 통해 리액트는 뷰 렌더링 오버헤드를 최소화한다는 사실을 알 수 있다.
+ */
+setInterval(() => {
+  root.render(
+    <p>현재 시각: {(new Date()).toLocaleString()}</p>
+  );
+}, 1000);
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
 reportWebVitals();
